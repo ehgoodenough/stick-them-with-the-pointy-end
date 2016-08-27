416b94141ff5a3d90b1fe392280010cfdfb127bd
Paul Muren <paulmuren@gmail.com>
2016-08-27 13:46:52 -0700

    Added getDistance function for geometry with two points

--- source/scripts/utility/Geometry.js
+++ source/scripts/utility/Geometry.js
@@ -2,6 +2,10 @@ export default class Geometry {
     static getMagnitude(x, y) {
         return Math.sqrt(x*x + y*y)
     }
+    static getDistance(point1, point2) {
+        var resultVector = {x: point2.x - point1.x, y: point2.y - point1.y}
+        return Geometry.getMagnitude(resultVector.x, resultVector.y)
+    }
     static getAngle(x, y) {
         var angle = Math.atan2(y, x)
         if(angle < 0) {
