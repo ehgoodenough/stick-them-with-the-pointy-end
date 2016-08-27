a9a5e3730385083c82476d165a2ffbda76f3a340
Paul Muren <paulmuren@gmail.com>
2016-08-26 23:46:18 -0700

    Added Geometry class and character rotation, plus basic non-standard gamepad compatibility

--- source/index.js
+++ source/index.js
@@ -54,15 +54,33 @@ class Hero extends Pixi.Sprite {
 
         var gamepads = navigator.getGamepads()
         if(gamepads[0] != undefined) {
-            var x = gamepads[0].axes[0]
-            var y = gamepads[0].axes[1]
+            var axes = gamepads[0].axes.slice();
+            if(axes.length == 5){
+                axes.shift()
+            }
+            var x = axes[0]
+            var y = axes[1]
 
             var magnitude = Math.sqrt(x*x + y*y)
             if(magnitude > 0.05) {
-                console.log("!!")
-                this.rotation
+                this.rotation = Geometry.getAngle(x, y)
             }
         }
+    }
+}
+
+class Geometry{
+    static getMagnitude(vector){
+        return Math.sqrt(vector.y*vector.y + vector.x*vector.x)
+    }
+
+    static getAngle(x, y){
+        var angle = Math.atan2(y, x)
+        if(angle < 0){
+            angle += Math.PI*2
+        }
+        angle -= Math.PI/2
+        return angle
     }
 }
 
