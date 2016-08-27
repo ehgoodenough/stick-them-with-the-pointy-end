a74b36bbe7bc4aa5d79549b6d8efe0b5ee044e82
Andrew McPherson <andrewmcp333@gmail.com>
2016-08-27 01:04:44 -0700

    Added a level loader

--- source/scripts/GameContainer.js
+++ source/scripts/GameContainer.js
@@ -5,14 +5,30 @@ import config from "config.js"
 import Hero from "scripts/Hero.js"
 import Tile from "scripts/Tile.js"
 
+var LEVEL = require("raw!levels/dungeon.txt")
+var tiles = LEVEL.split("\n").map((row) => {
+    return row.split("")
+})
+
 export default class GameContainer extends Pixi.Container {
     constructor() {
         super()
 
-        this.addChild(new Tile({tx: 0, ty: 0}))
-        this.addChild(new Tile({tx: 1, ty: 0}))
-        this.addChild(new Tile({tx: 0, ty: 1}))
+        for(var ty = 0; ty < tiles.length; ty += 1) {
+            for(var tx = 0; tx < tiles[ty].length; tx+= 1) {
+                var tile = tiles[ty][tx]
+                if(tile == "#") {
+                    this.addChild(new Tile({
+                        tx: tx, ty: ty
+                    }))
+                }
+            }
+        }
+
         this.addChild(new Hero())
+
+        // this.scale.x /= 2
+        // this.scale.y /= 2
     }
     addChild(child) {
         super.addChild(child)
