aee9a3c663183e1e86c8f4133dfd98eafb936cf0
Paul Muren <paulmuren@gmail.com>
2016-08-28 12:36:13 -0700

    Added floors

---
+++ source/scripts/models/Floor.js
@@ -0,0 +1,27 @@
+import Pixi from "pixi.js"
+import config from "config.js"
+
+var FLOOR_TEXTURES = [
+    Pixi.Texture.fromImage(require("images/floor1.png"))
+    // Pixi.Texture.fromImage(require("images/hero.png")),
+    // Pixi.Texture.fromImage(require("images/hero.png")),
+    // Pixi.Texture.fromImage(require("images/hero.png")),
+    // Pixi.Texture.fromImage(require("images/hero.png")),
+    // Pixi.Texture.fromImage(require("images/hero.png")),
+    // Pixi.Texture.fromImage(require("images/hero.png"))
+]
+
+export default class Hero extends Pixi.Sprite {
+    constructor(floor) {
+        super(FLOOR_TEXTURES[floor.textureIndex])
+        this.textureIndex = floor.textureIndex
+        this.spawnPosition = {tx: floor.tx, ty: floor.tx}
+        this.anchor.x = 0
+        this.anchor.y = 0
+        this.position.x = (floor.tx + this.anchor.x) * config.tile.size
+        this.position.y = (floor.ty + this.anchor.y) * config.tile.size
+    }
+    getData(){
+        return {tx: this.spawnPosition.tx, ty: this.spawnPosition.ty, textureIndex: this.textureIndex}
+    }
+}
